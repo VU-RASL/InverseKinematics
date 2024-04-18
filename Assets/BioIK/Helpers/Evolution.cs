97a3f81c954df4e90187713a2f6f525ae79aff16
IMOsbo <107141169+IMOsbo@users.noreply.github.com>
2024-04-18 08:46:24 -0500

    Added nullspace calculations :)

--- Assets/BioIK/Helpers/Evolution.cs
+++ Assets/BioIK/Helpers/Evolution.cs
@@ -1,6 +1,9 @@
-﻿using UnityEngine;
+using UnityEngine;
 using System.Threading;
 using System.Collections.Generic;
+using MathNet.Numerics;
+using MathNet.Numerics.LinearAlgebra.Double;
+using MathNet.Numerics.LinearAlgebra;
 
 namespace BioIK {
 
@@ -655,6 +658,10 @@ namespace BioIK {
         private double NewF;
         private double[] NewG;
 
+        private Matrix<double> empty = Matrix<double>.Build.Dense(7,7,0);
+        // private Matrix<double> identity = Matrix<double>.Build.DenseIdentity(3);
+        private Matrix<double> identity = Matrix<double>.Build.DenseIdentity(3);
+
         public BFGS(int dimensionality, System.Func<double[], double> function, System.Func<double[], double[]> gradient) {
             Dimensionality = dimensionality;
             Function = function;
@@ -715,10 +722,106 @@ namespace BioIK {
                     }
                 }
             }
+            
+            
+            var jacobian = ComputeJacobian(Solution[0], Solution[1], Solution[2], Solution[3], Solution[4], Solution[5], Solution[6]);
+            var nullspace = jacobian.Multiply(jacobian.PseudoInverse());
+            var subtraction = identity.Subtract(nullspace);
+            // Debug.Log(subtraction);
+            // Debug.Log(nullspace);
+            // Debug.Log(string.Join(" ", G));
+            // Debug.Log(nullspace);
 
             Value = Function(Solution);
         }
 
+        private double ConvertRadians(double theta) {
+            return theta * System.Math.PI / 180;
+        }
+
+        private DenseMatrix ComputeJacobian(double theta1, double theta2, double theta3, double theta4, double theta5, double theta6, double theta7)
+        {   
+            // Debug.Log("When you're done testing, uncomment me!!");
+
+            // theta1 = ConvertRadians(theta1);
+            // theta2 = ConvertRadians(theta2);
+            // theta3 = ConvertRadians(theta3);
+            // theta4 = ConvertRadians(theta4);
+            // theta5 = ConvertRadians(theta5);
+            // theta6 = ConvertRadians(theta6);
+            // theta7 = ConvertRadians(theta7);
+
+            // var jacobian = DenseMatrix.OfArray(new double[,] {{0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}});
+            var jacobian = DenseMatrix.OfArray(new double[,] {{0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}, {0,0,0,0,0,0,0}});
+
+            var sinTheta1 = System.Math.Sin(theta1);
+            var sinTheta3 = System.Math.Sin(theta3);
+            var sinTheta5 = System.Math.Sin(theta5);
+            var sinTheta1Theta2 = System.Math.Sin(theta1 + theta2);
+            var sinTheta3Theta4 = System.Math.Sin(theta3 + theta4);
+            var sinTheta6Theta7 = System.Math.Sin(theta6 + theta7);
+
+            var cosTheta3 = System.Math.Cos(theta3);
+            var cosTheta5 = System.Math.Cos(theta5);
+            var cosTheta1Theta2 = System.Math.Cos(theta1 + theta2);
+            var cosTheta3Theta4 = System.Math.Cos(theta3 + theta4);
+            var cosTheta6Theta7 = System.Math.Cos(theta6 + theta7);
+        
+            jacobian[0, 0] = -0.28504 * sinTheta1 + 0.28868 * sinTheta5 * cosTheta1Theta2 * cosTheta6Theta7 + 0.22499 * sinTheta5 * cosTheta1Theta2 + 0.28868 * sinTheta1Theta2 * sinTheta3Theta4 * sinTheta6Theta7 - 0.1067 * sinTheta1Theta2 * cosTheta3 - 0.28868 * sinTheta1Theta2 * cosTheta5 * cosTheta3Theta4 * cosTheta6Theta7 - 0.22499 * sinTheta1Theta2 * cosTheta5 * cosTheta3Theta4 - 0.1067 * sinTheta1Theta2;
+            jacobian[1, 0] = 0.28868 * sinTheta5 * sinTheta1Theta2 * cosTheta6Theta7 + 0.22499 * sinTheta5 * sinTheta1Theta2 - 0.28868 * sinTheta3Theta4 * sinTheta6Theta7 * cosTheta1Theta2 + 0.28504 * System.Math.Cos(theta1) + 0.1067 * cosTheta3 * cosTheta1Theta2 + 0.28868 * cosTheta5 * cosTheta1Theta2 * cosTheta3Theta4 * cosTheta6Theta7 + 0.22499 * cosTheta5 * cosTheta1Theta2 * cosTheta3Theta4 + 0.1067 * cosTheta1Theta2;
+            jacobian[2, 0] = 0;
+            // jacobian[3, 0] = 0;
+            // jacobian[4, 0] = 0;
+            // jacobian[5, 0] = 1;
+
+            jacobian[0, 1] = 0.28868 * sinTheta5 * cosTheta1Theta2 * cosTheta6Theta7 + 0.22499 * sinTheta5 * sinTheta1Theta2 + 0.28868 * sinTheta1Theta2 * sinTheta3Theta4 * sinTheta6Theta7 - 0.1067 * sinTheta1Theta2 * cosTheta3 - 0.28868 * sinTheta1Theta2 * cosTheta5 * cosTheta3Theta4 * cosTheta6Theta7 - 0.22499 * sinTheta1Theta2 * cosTheta5 * cosTheta3Theta4 - 0.1067 * sinTheta1Theta2;
+            jacobian[1, 1] = 0.28868 * sinTheta5 * sinTheta1Theta2 * cosTheta6Theta7 + 0.22499 * sinTheta5 * sinTheta1Theta2 - 0.28868 * sinTheta3Theta4 * sinTheta6Theta7 * cosTheta1Theta2 + 0.1067 * cosTheta1Theta2 * cosTheta3 + 0.28868 * cosTheta5 * cosTheta1Theta2  * cosTheta3Theta4 * cosTheta6Theta7 + 0.22499 * cosTheta1Theta2 * cosTheta5 * cosTheta3Theta4 + 0.1067 * cosTheta1Theta2;
+            jacobian[2, 1] = 0;
+            // jacobian[3, 1] = 0;
+            // jacobian[4, 1] = 0;
+            // jacobian[5, 1] = 1;
+
+            jacobian[0, 2] = -1 * (0.1067 * sinTheta3 + 0.28868 * sinTheta3Theta4 * cosTheta5 * cosTheta6Theta7 + 0.22499 * sinTheta3Theta4 * cosTheta5 + 0.28868 * sinTheta6Theta7 * cosTheta3Theta4) * cosTheta1Theta2;
+            jacobian[1, 2] = -1 * (0.1067 * sinTheta3 + 0.28868 * sinTheta3Theta4 * cosTheta5 * cosTheta6Theta7 + 0.22499 * sinTheta3Theta4 * cosTheta5 + 0.28868 * sinTheta6Theta7 * cosTheta3Theta4) * sinTheta1Theta2;
+            jacobian[2, 2] = -0.28868 * sinTheta3Theta4 * sinTheta6Theta7 + 0.1067 * cosTheta3 + 0.28868 * cosTheta5 * cosTheta3Theta4 * cosTheta6Theta7 + 0.22499 * cosTheta5 * cosTheta3Theta4;
+            // jacobian[3, 2] = sinTheta1Theta2;
+            // jacobian[4, 2] = -1 * cosTheta1Theta2;
+            // jacobian[5, 2] = 0;
+
+            jacobian[0, 3] = -1 * (0.28868 * sinTheta3Theta4 * cosTheta5 * cosTheta6Theta7 + 0.22499 * sinTheta3Theta4 * cosTheta5 + 0.28868 * sinTheta6Theta7 * cosTheta3Theta4) * cosTheta1Theta2;
+            jacobian[1, 3] = -1 * (0.28868 * sinTheta3Theta4 * cosTheta5 * cosTheta6Theta7 + 0.22499 * sinTheta3Theta4 * cosTheta5 + 0.28868 * sinTheta6Theta7 * cosTheta3Theta4) * sinTheta1Theta2;
+            jacobian[2, 3] = -0.28868 * sinTheta3Theta4 * sinTheta6Theta7 + 0.28868 * cosTheta5 * cosTheta3Theta4 * cosTheta6Theta7 + 0.22499 * cosTheta5 * cosTheta3Theta4;
+            // jacobian[3, 3] = sinTheta1Theta2;
+            // jacobian[4, 3] = -1 * cosTheta1Theta2;
+            // jacobian[5, 3] = 0;
+
+            jacobian[0, 4] = -0.28868 * sinTheta5 * cosTheta1Theta2 * cosTheta3Theta4 * cosTheta6Theta7 - 0.22499 * sinTheta5 * cosTheta1Theta2 * cosTheta3Theta4 + 0.28868 * sinTheta1Theta2 * cosTheta5 * cosTheta6Theta7 + 0.22499 * sinTheta1Theta2 * cosTheta5;
+            jacobian[1, 4] = -0.28868 * sinTheta5 * sinTheta1Theta2 * cosTheta3Theta4 * cosTheta6Theta7 - 0.22499 * sinTheta5 * sinTheta1Theta2 * cosTheta3Theta4 - 0.28868 * cosTheta1Theta2 * cosTheta5 * cosTheta6Theta7 - 0.22499 * cosTheta1Theta2 * cosTheta5;
+            jacobian[2, 4] = -1 * (0.28868 * cosTheta6Theta7 + 0.22499) * sinTheta5 * sinTheta3Theta4;
+            // jacobian[3, 4] = sinTheta3Theta4 * cosTheta1Theta2;
+            // jacobian[4, 4] = sinTheta1Theta2 * sinTheta3Theta4;
+            // jacobian[5, 4] = -1 * cosTheta3Theta4;
+
+            jacobian[0, 5] = -0.28868 * sinTheta5 * sinTheta1Theta2 * sinTheta6Theta7 - 0.28868 * sinTheta3Theta4 * cosTheta1Theta2 * cosTheta6Theta7 - 0.28868 * sinTheta6Theta7 * cosTheta5 * cosTheta1Theta2 * cosTheta3Theta4;
+            jacobian[1, 5] = 0.28868 * sinTheta5 * sinTheta6Theta7 * cosTheta1Theta2 - 0.28868 * sinTheta1Theta2 * sinTheta3Theta4 * cosTheta6Theta7 - 0.28868 * sinTheta1Theta2 * sinTheta6Theta7 * cosTheta5 * cosTheta3Theta4;
+            jacobian[2, 5] = -0.28868 * sinTheta3Theta4 * sinTheta6Theta7 * cosTheta5 + 0.28868 * cosTheta3Theta4 * cosTheta6Theta7;
+            // jacobian[3, 5] = -1 * sinTheta5 * cosTheta1Theta2 * cosTheta3Theta4 + sinTheta1Theta2 * cosTheta5;
+            // jacobian[4, 5] = -1 * sinTheta5 * sinTheta1Theta2 * cosTheta3Theta4 - cosTheta5 * cosTheta1Theta2;
+            // jacobian[5, 5] = -1 * sinTheta5 * sinTheta3Theta4;
+
+            jacobian[0, 6] = -0.28868 * sinTheta5 * sinTheta1Theta2 * sinTheta6Theta7 - 0.28868 * sinTheta3Theta4 * cosTheta1Theta2 * cosTheta6Theta7 - 0.28868 * sinTheta6Theta7 * cosTheta5 * cosTheta1Theta2 * cosTheta3Theta4;
+            jacobian[1, 6] = 0.28868 * sinTheta5 * sinTheta6Theta7 * cosTheta1Theta2 - 0.28868 * sinTheta1Theta2 * sinTheta3Theta4 * cosTheta6Theta7 - 0.28868 * sinTheta1Theta2 * sinTheta6Theta7 * cosTheta5  * cosTheta3Theta4;
+            jacobian[2, 6] = -0.28868 * sinTheta3Theta4 * sinTheta6Theta7 * cosTheta5 + 0.28868 * cosTheta3Theta4 * cosTheta6Theta7;
+            // jacobian[3, 6] = -1 * sinTheta5 * cosTheta1Theta2 * cosTheta3Theta4 + sinTheta1Theta2 * cosTheta5;
+            // jacobian[4, 6] = -1 * sinTheta5 * sinTheta1Theta2 * cosTheta3Theta4 - cosTheta5 * cosTheta1Theta2 ;
+            // jacobian[5, 6] = -1 * sinTheta5 * sinTheta3Theta4;
+
+            return jacobian;
+        }
+
+        private Matrix<double> ComputeInverse(DenseMatrix j) {
+            return j.PseudoInverse();
+        }
         public void Minimise(double[] values, double timeout) {
             for(int i=0; i<Dimensionality; i++) {
                 Solution[i] = values[i];
@@ -754,6 +857,15 @@ namespace BioIK {
                 }
             }
 
+            var jacobian = ComputeJacobian(Solution[0], Solution[1], Solution[2], Solution[3], Solution[4], Solution[5], Solution[6]);
+            var nullspace = jacobian.Multiply(jacobian.PseudoInverse());
+            var subtraction = identity.Subtract(nullspace);
+            Debug.Log(subtraction * 0.25);
+            // Solution += step * nullspace * gradient;
+            // need to multiply this gradient by the solution... 
+            // gradient + nullspace 
+            // Debug.Log(newGradient);
+            
             Value = Function(Solution);
         }
 
